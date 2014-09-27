5bf0b18fb46ff04a79033597567489c66e57abc1
Andy Wong <alacritythief@gmail.com>
2014-09-27 19:40:24 -0400

    refactored some code, adjusted styling, added gemfiles.

--- server.rb
+++ server.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
-require 'sinatra/reloader'
-require 'pry'
 
+# MANNERISMS
 
 def greeting
   @answer = "<strong>Hello!</strong><br> I'm Sir Quackington,<br>adorable lord of the sea.<br>Ask me anything,<br>
@@ -15,7 +14,7 @@ def random
             "<strong><em>PLEASE</em></strong> stop abusing the ask button.",
             "Did you forget to add a semicolon?",
             "Maybe you forgot a comma somewhere?",
-            "Maybe you added an 's' to something by mistake in ActiveRecord.",
+            "Maybe you added an 's' to something<br>by mistake in ActiveRecord.",
             "Maybe you should take a walk?",
             "I didn't quite understand that, but let me tell you, life is great.",
             "Take care of yourself.",
@@ -45,6 +44,8 @@ post'/' do
     else
       @answer = "QUACK " * @number
     end
+  elsif @question.first.include? "ruby"
+    @answer = "Have you checked the <a href='http://www.ruby-doc.org/core-2.1.3/' target='_blank'>Ruby</a> docs?"
   elsif @question.first.include? "google"
     @question.shift
     @statement = @question.join(" ")
@@ -56,7 +57,10 @@ post'/' do
     @query = @question.join("+")
     @answer = "#{@statement}? <br> <a href='http://stackoverflow.com/search?q=#{@query}' target='_blank'>Let's look at Stack Overflow.</a>"
   elsif @question.first.include? "help"
-    @answer = "Okay! Let me help you.<br> First words are always important<br>when talking to a duck.<br><br><strong>WIKIPEDIA</strong> <em>something</em><br><strong>GOOGLE</strong> <em>something</em><br><strong>OVERFLOW</strong> <em>programming questions</em><br><br>You can even ask me to<br><strong>QUACK</strong> <em>X</em> amount of times!<br><br>I will know even more soon!"
+    @answer = "<p>Okay! Let me help you.</p>
+              <p>First words are always important<br>when talking to a duck.</p>
+              <p><strong>WIKIPEDIA</strong> <em>something</em><br><strong>GOOGLE</strong> <em>something</em><br><strong>OVERFLOW</strong> <em>programming questions</em><br><br>You can even ask me to<br><strong>QUACK</strong> <em>X</em> amount of times!</p>
+              <p>I will know even more soon!</p>"
   elsif @question.first.include? "wikipedia"
     @question.shift
     @statement = @question.join(" ")
